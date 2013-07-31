d07430808e305dc1c452c1ab4cbcc505cf1eecb8
arun.a <arun.mudhaliar@gmail.com>
2013-07-31 18:49:21 +0530

    adding scripts

---
+++ MonoGEAR/object3d.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using MonoGEAR;
+
+namespace MonoGEAR
+{
+    public sealed class object3d
+    {
+        private IntPtr m_pObj3dPtr;
+
+        public object3d(IntPtr ptr)
+        {
+            m_pObj3dPtr = ptr;
+        }
+
+        public static object3d create(string name)
+        {
+            return new object3d(MonoGEAREntryPointClass.engine_loadAndAppendFBX(MonoGEAREntryPointClass.engine_getWorld(0), name));
+        }
+    }
+}

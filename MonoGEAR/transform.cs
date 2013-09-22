4e71b46d041361dba4aa52e4fa280720df5fdde4
arun.a <arun.mudhaliar@gmail.com>
2013-09-22 12:45:32 +0530

    adding new file

---
+++ MonoGEAR/transform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoGEAR
+{
+    public class transform
+    {
+        protected IntPtr m_pObj3dPtr;
+
+        public transform(IntPtr ptr)
+        {
+            m_pObj3dPtr = ptr;
+        }
+
+        protected IntPtr getHandle()
+        {
+            return m_pObj3dPtr;
+        }
+    }
+}

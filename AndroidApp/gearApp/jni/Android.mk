addb5aa17abd1bb7910b9bbff1d6aa54b33aff15
arun.a <arun.mudhaliar@gmail.com>
2014-05-01 22:46:35 +0530

    [GEARAndroid][Project structuring]

--- AndroidApp/gearApp/jni/Android.mk
+++ AndroidApp/gearApp/jni/Android.mk
@@ -1,63 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE    := GEAREngine.dll
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../../../GEAREngine/src/physics $(LOCAL_PATH)/../../../GEAREngine/src
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lOpenSLES
-	
-LOCAL_SRC_FILES := \
-	../../../GEAREngine/src/zlib-1.2.4/adler32.c \
-	../../../GEAREngine/src/zlib-1.2.4/compress.c \
-	../../../GEAREngine/src/zlib-1.2.4/crc32.c \
-	../../../GEAREngine/src/zlib-1.2.4/deflate.c \
-	../../../GEAREngine/src/zlib-1.2.4/infback.c \
-	../../../GEAREngine/src/zlib-1.2.4/inffast.c \
-	../../../GEAREngine/src/zlib-1.2.4/inflate.c \
-	../../../GEAREngine/src/zlib-1.2.4/inftrees.c \
-	../../../GEAREngine/src/zlib-1.2.4/trees.c \
-	../../../GEAREngine/src/zlib-1.2.4/uncompr.c \
-	../../../GEAREngine/src/zlib-1.2.4/zutil.c \
-	../../../GEAREngine/src/GEAREngine.cpp     \
-	../../../GEAREngine/src/audio/SoundEngine.cpp     \
-	../../../GEAREngine/src/audio/SoundSample.cpp     \
-	../../../GEAREngine/src/audio/SoundSource.cpp     \
-	../../../GEAREngine/src/core/Camera.cpp     \
-	../../../GEAREngine/src/core/fbo.cpp     \
-	../../../GEAREngine/src/core/gxAnimation.cpp     \
-	../../../GEAREngine/src/core/gxAnimationSet.cpp     \
-	../../../GEAREngine/src/core/gxAnimationTrack.cpp     \
-	../../../GEAREngine/src/core/gxCamera.cpp     \
-	../../../GEAREngine/src/core/gxDebug.cpp     \
-	../../../GEAREngine/src/core/gxLight.cpp     \
-	../../../GEAREngine/src/core/gxMaterial.cpp     \
-	../../../GEAREngine/src/core/gxMesh.cpp     \
-	../../../GEAREngine/src/core/gxSkinnedMesh.cpp     \
-	../../../GEAREngine/src/core/gxSurfaceShader.cpp     \
-	../../../GEAREngine/src/core/gxWorld.cpp     \
-	../../../GEAREngine/src/core/object3d.cpp     \
-	../../../GEAREngine/src/core/physicsEngine.cpp     \
-	../../../GEAREngine/src/core/TextureManager.cpp     \
-	../../../GEAREngine/src/core/transform.cpp     \
-	../../../GEAREngine/src/hwShader/gxHWShader.cpp     \
-	../../../GEAREngine/src/hwShader/HWShaderManager.cpp     \
-	../../../GEAREngine/src/renderer/gxRenderer.cpp     \
-	../../../GEAREngine/src/tga/TGALoader.cpp     \
-	../../../GEAREngine/src/util/gxCrc32.cpp     \
-	../../../GEAREngine/src/util/gxFile.cpp     \
-	../../../GEAREngine/src/util/gxUtil.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE    := monomodule
-LOCAL_SRC_FILES := ../monoandroid-arm/lib/libmonosgen-2.0.a
-LOCAL_EXPORT_C_INCLUDES := monoandroid-arm/include/mono-2.0
+LOCAL_SRC_FILES := ../../gearengineAndroid/monoandroid-arm/lib/libmonosgen-2.0.a
+LOCAL_EXPORT_C_INCLUDES := ../gearengineAndroid/monoandroid-arm/include/mono-2.0
 include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := gearmodule
+LOCAL_SRC_FILES := ../../gearengineAndroid/libs/armeabi/libGEAREngine.dll.so
+include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := gearapp
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lOpenSLES
-LOCAL_STATIC_LIBRARIES := monomodule GEAREngine.dll android_native_app_glue
+LOCAL_STATIC_LIBRARIES := monomodule android_native_app_glue
+LOCAL_SHARED_LIBRARIES := gearmodule
 LOCAL_SRC_FILES := gearapp.cpp \
 	../../../GUIInternal/mono/src/monoWrapper.cpp
 APP_OPTIM := debug
